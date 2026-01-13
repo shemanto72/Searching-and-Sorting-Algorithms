b2148c211a361f930c9bb0619912d538e3a32e20
shemanto72 <mdshemanto1020@gmail.com>
2026-01-13 12:38:38 +0600

    ok

--- index.py
+++ index.py
@@ -8,7 +8,7 @@ def main():
     if chosen == "1":
         searching()
     elif chosen == "2":
-        print("Sorting chosen")
+        sorting()
     else:
         print("Invalid Input")
 
@@ -16,10 +16,12 @@ def searching():
     print("***************************")
     print("1.Binary Search")
     print("2.Linear Search")
-    chosen = input("You have Chosen Searching! Now chose what algorithim you want to use: ")
+    print("3.Interpolation Search")
+    chosen = input("You have Chosen Searching! Now chose what algorithm you want to use: ")
     if chosen == "1":
         nums = list(map(int, input("***************************** \nYou have choose binary search, first enter the number list: ").split()))
-        target = int(input("Enter the Targated value: "))
+        nums.sort()
+        target = int(input("Enter the Targeted value: "))
         result = binary_search(nums , target)
         if result == -1:
             print("Target not Found!")
@@ -27,29 +29,46 @@ def searching():
             print(f'The index number of {target} is: {result}')
     elif chosen == "2":
         nums = list(map(int, input("***************************** \nYou have choose binary search, first enter the number list: ").split()))
-        target = int(input("Enter The targated value: "))
+        target = int(input("Enter The targeted value: "))
         result = linear_search(nums , target)
         if result == -1:
             print("Target not found!")
         else:
             print(f'The index number of {target} is : {result}')
+    elif chosen == "3":
+        nums = list(map(int, input("***************************** \nYou have choose Interpolation search, first enter the number list: ").split()))
+        nums.sort()
+        target = int(input("Enter The targeted value: "))
+        result = interpolation_search(nums , target)
+        if result == -1:
+            print("Target not found!")
+        else:
+            print(f'The index number of {target} is : {result}')
     else:
-        print("Invalid Choise")
+        print("Invalid Choice")
 
 def sorting():
-    chose = input("You have Chosen sorting! Now choose what algorithim you want to use: ")
     print("1.Bubble Sort")
     print("2.Selection Sort")
+    print("3.Merge Sort")
+    chose = input("You have Chosen sorting! Now choose what algorithim you want to use: ").strip()
 
-    if chose == "1":
-        pass
-    elif chose == "2":
-        pass
+    if chose == "2":
+        nums = list(map(int, input("***************************** \nYou have choose selection sort, first enter the number list: ").split()))
+        selection_sort(nums)
+        print(f'Sorted List: {nums}')
+    elif chose == "1":
+        nums = list(map(int, input("***************************** \nYou have choose bubble sort, first enter the number list: ").split()))
+        bubble_sort(nums)
+        print(f'Sorted List: {nums}')
+    elif chose == "3":
+        nums = list(map(int, input("***************************** \nYou have choose merge sort, first enter the number list: ").split()))
+        print(f'Sorted List: {merge_sort(nums)}')
     else:
-        print("Invalid Choise")
+        print("Invalid Choice")
 
 def binary_search(nums, target):
-    top = len(nums)
+    top = len(nums) - 1
     low = 0
     print(nums)
     while low <= top:
@@ -69,7 +88,67 @@ def linear_search(nums , target):
     return -1
 
 
+def interpolation_search(nums, target):
+    low = 0
+    high = len(nums) - 1
 
+    while low <= high and nums[low] <= target <= nums[high]:
+        if nums[high] == nums[low]:
+            if nums[low] == target:
+                return low
+            else:
+                break
+        possible_index = low + (target - nums[low]) * (high - low) // (nums[high] - nums[low])
+        if nums[possible_index] == target:
+            return possible_index
+        elif nums[possible_index] > target:
+            high = possible_index - 1
+        else:
+            low = possible_index + 1
+    return -1
+
+
+
+def selection_sort(nums):
+    for i in range(len(nums)):
+        for j in range(i+1 , len(nums)):
+            if nums[j] < nums[i]:
+                nums[j] , nums[i] = nums[i] , nums[j]
+
+
+def bubble_sort(nums):
+    for i in range(len(nums)):
+        for j in range(len(nums)-1-i):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+
+
+
+def merge_sort(nums):
+    if len(nums) <= 1:
+        return nums
+
+    mid = len(nums) // 2
+    left = merge_sort(nums[:mid])
+    right = merge_sort(nums[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
 
 main()
 
